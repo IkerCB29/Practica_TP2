a79603bc02ff02e4745ba100dbac8c9eb006b45c
David <dmonto01@ucm.es>
2022-03-10 10:09:45 +0100

    Arreglado un cambio en el constructor

--- src/simulator/model/NewJunctionEvent.java
+++ src/simulator/model/NewJunctionEvent.java
@@ -3,7 +3,7 @@ package simulator.model;
 public class NewJunctionEvent extends Event{
 
 	private Junction jun;
-	private int xCoor, yCoord;
+	private int xCoor, yCoor;
 	private String id;
 	private LightSwitchingStrategy lsStrategy;
 	private DequeuingStrategy dqStrategy;
@@ -11,12 +11,16 @@ public class NewJunctionEvent extends Event{
 	public NewJunctionEvent(int time, String id, LightSwitchingStrategy 
 						lsStrategy, DequeuingStrategy dqStrategy, int xCoor, int yCoord) {
 		super(time);
-		
+		this.id = id;
+		this.lsStrategy = lsStrategy;
+		this.dqStrategy = dqStrategy;
+		this.xCoor = xCoor;
+		this.yCoor = yCoord;
 	}
 
 	@Override
 	void execute(RoadMap map) {
-		jun = new Junction(id, lsStrategy, dqStrategy, xCoor, yCoord);
+		jun = new Junction(id, lsStrategy, dqStrategy, xCoor, yCoor);
 		map.addJunction(jun);	
 	}
 	@Override
